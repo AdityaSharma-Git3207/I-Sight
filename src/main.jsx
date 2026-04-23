f8cd97bb24640b528d8ec2ff904b8dd7543c9f65
AdityaSharma-Git3207 <aadee.bharat@gmail.com>
2026-04-23 21:52:58 +0530

    feat: add automatic scroll to top on route change

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App";
 import Consultation from "./pages/Consultation";
 import Store from "./pages/Store";
 import Awareness from "./pages/Awareness";
+import ScrollToTop from "./components/ScrollToTop";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+
+    <ScrollToTop />
+
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/consultation" element={<Consultation />} />
       <Route path="/store" element={<Store />} />
       <Route path="/awareness" element={<Awareness />} />
     </Routes>
+
   </BrowserRouter>
 );
